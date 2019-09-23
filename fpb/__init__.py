8efb8ee91a29c479127daaf48421410ff865a99d
Anthony Monthe <anthony.monthe@gmail.com>
2019-09-23 02:53:25 +0100

    Added meta

--- fpb/__init__.py
+++ fpb/__init__.py
@@ -1,4 +1,9 @@
+"""
+FPB - Floating Point Benchmark
+Run tests about math function with different frameworks.
+"""
 VERSION = (0, 1)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Anthony Monthe (ZuluPro)'
-__email__ = 'anthony.monthe@gmail.com'
+__email__ = 'amonthe@cloudspectator.com'
+__url__ = 'https://github.com/cloudspectatordevelopment/python-fpb'
